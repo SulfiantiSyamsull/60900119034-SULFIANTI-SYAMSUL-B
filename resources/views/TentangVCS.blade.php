a968244ef9fc3931e3f9a5243795e2803652b8f5
LAPTOP-EUDTF4TO\Sulfianti <60900119034@uin-alauddin.ac.id>
2021-11-09 12:00:00 +0700

    finish

---
+++ resources/views/TentangVCS.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Pengertian VCS</p>
+    <p> Version control adalah sebuah sistem yang merekam perubahan-perubahan dari sebuah berkas
+atau sekumpulan berkas dari waktu ke waktu sehingga Anda dapat menilik kembali versi khusus
+suatu saat nanti.</p>
+    <li>1. GIT</li>
+    <li>2. CVS</li>
+    <li>3. SVN</li>
+    <li>4. Mercurial</li>
+    <li>5. Bazaar</li>
+</body>
+</html>
